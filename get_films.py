e14523e96266f42bbaaee3f8fd1cc273cd19854e
Olesya Lekareva <108861025+olesya098@users.noreply.github.com>
2024-10-16 20:33:37 +0300

    Add files via upload

---
+++ get_films.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, jsonify
+
+from db import get_connection
+
+get_films_blueprint = Blueprint('get_films', __name__)
+
+
+@get_films_blueprint.route('/api/v1/films', methods=['GET'])
+def get_films():
+    global conn, cur
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute('SELECT * FROM film')
+
+        films = cur.fetchall()
+
+        films_json = []
+        for film in films:
+            cur.execute(f'SELECT title FROM Category_film WHERE Category_film_id={film[7]}')
+            category_film = cur.fetchone()[0]
+
+            films_json.append(
+                {
+                    'id': film[0],
+                    'image': film[1],
+                    'title': film[2],
+                    'rate': film[3],
+                    'age': film[4],
+                    'year': film[5],
+                    'country': film[6],
+                    'category_film': category_film,
+                    'description': film[8],
+                    'link': film[9]
+                }
+            )
+
+        return jsonify(films_json)
+    except:
+        return jsonify({'message': 'error'})
+    finally:
+        cur.close()
+        conn.close()
