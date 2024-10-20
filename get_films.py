3935261dd571de2c5de78ffc7b7a662853f8e513
Olesya Lekareva <108861025+olesya098@users.noreply.github.com>
2024-10-20 20:02:42 +0300

    Update get_films.py

--- get_films.py
+++ get_films.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 
 from db import get_connection
 
-get_films_blueprint = Blueprint('get_films', __name__)
+get_films_blueprint = Blueprint('get_films', name)
 
 
 @get_films_blueprint.route('/api/v1/films', methods=['GET'])
@@ -37,6 +37,41 @@ def get_films():
             )
 
         return jsonify(films_json)
+    except:
+        return jsonify({'message': 'error'})
+    finally:
+        cur.close()
+        conn.close()
+
+
+@get_films_blueprint.route('/api/v1/films/<int:film_id>', methods=['GET'])
+def get_film(film_id):
+    global conn, cur
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute(f'SELECT * FROM film WHERE film_id={film_id}')
+
+        film = cur.fetchone()
+
+        cur.execute(f'SELECT title FROM Category_film WHERE Category_film_id={film[7]}')
+        category_film = cur.fetchone()[0]
+
+        film_json = {
+            'id': film[0],
+            'image': film[1],
+            'title': film[2],
+            'rate': film[3],
+            'age': film[4],
+            'year': film[5],
+            'country': film[6],
+            'category_film': category_film,
+            'description': film[8],
+            'link': film[9]
+        }
+
+        return jsonify(film_json)
     except:
         return jsonify({'message': 'error'})
     finally:
